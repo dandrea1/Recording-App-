dc6901905def87d5d1005f7112ce0f92a6523f51
Dana Andrea <danalandrea@gmail.com>
2019-09-11 08:55:04 -0500

    Added Second View Controller

---
+++ Sean_Fiverr/PlayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayViewController.swift
+//  Sean_Fiverr
+//
+//  Created by Dana Andrea on 9/10/19.
+//  Copyright © 2019 Dana Andrea. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
